ec0337b9c0bba3800e46b2b36dca9d0aa63669d5
Tiago Bastos Carneiro <tiagohard2@gmail.com>
2021-06-09 23:53:19 -0300

    criacao do projeto

---
+++ api-rest-clientes/src/test/java/com/br/builders/api/ApiRestClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.builders.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
